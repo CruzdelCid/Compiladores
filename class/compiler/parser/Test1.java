a52994e5a1363c5302a92b8db77ad3f65a282589
CruzdelCid <cldelcid@ufm.edu>
2022-10-21 13:36:00 -0600

    Impresion del arbol

--- class/compiler/parser/Test1.java
+++ class/compiler/parser/Test1.java
@@ -2,7 +2,9 @@ package parser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.StringReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +14,61 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 
 public class Test1 {
+    public static int verificar(Nodo raiz){
+
+        System.out.println(raiz.getNombre());
+        System.out.println(raiz.getNumNodo());
+        System.out.println(raiz.getValor());
+
+        return 1; 
+    }
+
+    public static void graficar(Nodo raiz){
+        FileWriter archivo = null;
+        PrintWriter pw = null;
+        String cadena = graficarNodo(raiz);
+
+        System.out.println(cadena);
+        
+        try{
+            archivo = new FileWriter("arbol.dot");
+            pw = new PrintWriter(archivo);
+            pw.println("digraph G {node[shape=box, style=filled, color=blanchedalmond]; edge[color=chocolate3];rankdir=UD \n");
+            pw.println(cadena);
+            pw.println("\n}");
+            archivo.close();
+            System.out.println("el achivo se genero --------a-a-a-a--a-a-a-a--a-a");
+        }catch (Exception e) {
+            System.out.println(e +" 1");
+        }
+        
+        try {
+            String cmd = "cmd /c dot -Tpng arbol.dot -o arbol.png";
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException ioe) {
+            System.out.println(ioe +" 2");
+        }
+        
+    }
+    
+    public static String graficarNodo(Nodo nodo){
+        String cadena = "";
+        
+        for(Nodo hijos : nodo.getHijos())
+        {
+            try {
+                cadena += "\"" + nodo.getNumNodo() + "_" + nodo.getNombre() + " -> " + nodo.getValor() + "\"->\"" + hijos.getNumNodo() + "_" + hijos.getNombre() + " -> " + hijos.getValor() + "\"\n";
+                cadena += graficarNodo(hijos);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            
+        }
+        
+        return cadena;
+    }
+
+
     public static void parsear(String filename) throws IOException {
         System.out.println(filename);
 
@@ -41,13 +98,14 @@ public class Test1 {
         
         try {
             s.parse();
-            // System.out.println();
             System.out.println("Analisis realizado correctamente");
 
         } catch (Exception ex) {
             Symbol sym = s.getS();
             System.out.println("Error de sintaxis. Linea: " + (sym.right) + " Columna: " + (sym.left) + ", Texto: \"" + sym.value + "\"");
         }
+
+        graficar(s.padre);
     }
 
     public static void main(String[] args) throws IOException {
